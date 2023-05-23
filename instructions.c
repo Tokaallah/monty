eac8e62cc5c7e8914006c91dc4b2d58c2f940f46
Tokaallah <tokallah.92@gmail.com>
2023-05-23 12:52:45 -0700

    fixing compile errors

--- instructions.c
+++ instructions.c
@@ -1,61 +1,52 @@
 #include "monty.h"
 #include <stdlib.h>
-
 /**
  * push - Pushes an element to the stack.
- * @stack: Double pointer to the stack.
- * @line_number: Line number in the Monty file.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number being interpreted from the Monty file.
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-    /* Check if the argument exists */
-    if (/* Argument does not exist */)
+    int value;
+
+    if (!stack)
+    {
+        fprintf(stderr, "L%u: Stack does not exist\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    if (!data || !data->arg)
     {
         fprintf(stderr, "L%u: usage: push integer\n", line_number);
         exit(EXIT_FAILURE);
     }
 
-    /* Convert the argument to an integer */
-    int value = /* Convert argument to integer using atoi() */;
+    value = atoi(data->arg);
 
-    /* Create a new node */
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
+    if (value == 0 && strcmp(data->arg, "0") != 0)
     {
-        fprintf(stderr, "Error: malloc failed\n");
+        fprintf(stderr, "L%u: usage: push integer\n", line_number);
         exit(EXIT_FAILURE);
     }
 
-    /* Assign values to the new node */
-    new_node->n = value;
-    new_node->prev = NULL;
-
-    if (*stack != NULL)
-    {
-        new_node->next = *stack;
-        (*stack)->prev = new_node;
-    }
-    else
-    {
-        new_node->next = NULL;
-    }
-
-    *stack = new_node;
+    add_node(stack, value);
 }
 
 /**
- * pall - Prints all the values on the stack.
- * @stack: Double pointer to the stack.
- * @line_number: Line number in the Monty file.
+ * pall - Prints all the values on the stack, starting from the top.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number being interpreted from the Monty file.
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
     stack_t *current = *stack;
 
-    /* Traverse the stack and print the values */
-    while (current != NULL)
+    (void)line_number;
+
+    while (current)
     {
         printf("%d\n", current->n);
         current = current->next;
     }
 }
+
--- monty.c
+++ monty.c
@@ -1,40 +1,39 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "monty.h"
 
 int main(int argc, char *argv[])
 {
-    /* Check if the correct number of arguments is provided */
+    FILE *file;
+    char *line = NULL;
+    size_t line_size = 0;
+    ssize_t line_length = 0;
+
     if (argc != 2)
     {
         fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
 
-    /* Open the file for reading */
-    FILE *file = fopen(argv[1], "r");
+    file = fopen(argv[1], "r");
     if (file == NULL)
     {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
-    /* Read the file line by line and process the instructions */
-    char *line = NULL;
-    size_t line_size = 0;
-    ssize_t line_length;
-    unsigned int line_number = 0;
-
     while ((line_length = getline(&line, &line_size, file)) != -1)
     {
-        line_number++;
-
-        /* Process the instruction in the line */
-
-        /* Tokenize the line and extract the opcode and argument */
-
-        /* Call the appropriate function based on the opcode */
-
-        /* Free allocated memory and move to the next line */
+        /* Process each line of the Monty file */
+        /* Your code here */
     }
 
-    /* Cleanup and close the file
+    free(line);
+    fclose(file);
+
+    return 0;
+}
 
